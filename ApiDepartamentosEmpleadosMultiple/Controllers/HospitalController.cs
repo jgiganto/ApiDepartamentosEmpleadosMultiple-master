29b96701b02b54f43c734497679f9239494daf21
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-04-08 21:50:06 +0200

    Add project files.

---
+++ ApiDepartamentosEmpleadosMultiple/Controllers/HospitalController.cs
@@ -0,0 +1,50 @@
+﻿using ApiDepartamentosEmpleadosMultiple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiDepartamentosEmpleadosMultiple.Controllers
+{
+    public class HospitalController : ApiController
+    {
+        ModeloHospital modelo;
+        public HospitalController()
+        {
+            this.modelo = new ModeloHospital();
+        }
+
+        [HttpGet]
+        [Route("api/MostrarDepartamentos")]
+        public List<DEPT> MostrarDepartamentos()
+        {
+            return modelo.GetDepartamentos();
+        }
+
+        [HttpGet]
+        [Route("api/EmpleadosDept")]
+        //http:/localhost:52896/api/EmpleadosDept/?deptnos=20&deptnos=30
+        public List<EMP> EmpleadosDepartamentos([FromUri]List<int?> deptnos)
+        {
+            if (deptnos != null)
+            {
+                return modelo.GetEmpleadosDepartamento(deptnos);
+            }
+            else { return null; }
+        }
+
+        [HttpPost]
+        [Route("api/Incremento")]
+        public List<EMP> IncrementoSalarial([FromUri]List<int?> empno,int? incremento)
+        {
+            if(empno != null && incremento != null)
+            {
+                return modelo.IncrementoSalarial(empno, incremento);
+            }
+            else { return null; }
+            
+        }
+    }
+}
---
+++ ApiDepartamentosEmpleadosMultiple/Models/ModeloHospital.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiDepartamentosEmpleadosMultiple.Models
+{
+    public class ModeloHospital
+    {
+        ContextoHospital contexto;
+        public ModeloHospital()
+        {
+            this.contexto = new ContextoHospital();
+        }
+        public List<DEPT> GetDepartamentos()
+        {
+            var consulta = from datos in contexto.DEPT
+                           select datos;
+            return consulta.ToList();
+        }
+        public List<EMP> GetEmpleadosDepartamento(List<int?> deptnos)
+        {
+            var consulta = from datos in contexto.EMP
+                           where deptnos.Contains(datos.DEPT_NO)
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public List<EMP> IncrementoSalarial(List<int?> empno,int? incremento)
+        {
+            var consulta = from datos in contexto.EMP
+                           where empno.Contains(datos.EMP_NO)
+                           select datos;
+            foreach(var emp in consulta)
+            {
+                emp.SALARIO += incremento;
+            }
+            contexto.SaveChanges();
+            return consulta.ToList();
+        }
+    }
+}
